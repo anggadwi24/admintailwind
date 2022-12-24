ae937766af9dc54b87657712ae35d695b8b4bc33
anggadwi14 <anggadwi1014@gmail.com>
2022-12-24 18:50:59 +0800

    nothing

--- pages/feature/detail/[slug].js
+++ pages/feature/detail/[slug].js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 import api from "../../../lib/Api";
 import MainLayout from "../../../layouts/MainLayout";
 import { useAuth } from "../../../contexts/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 
@@ -17,8 +17,35 @@ const Detail = (props) => {
   
     const [error,setError] = useState([]);
     const [loading,setLoading] = useState(false);
-   
+    const [message,setMessage] = useState(null);
+    const [showToast,setShowToast] = useState(false);
+    const [type,setType] = useState('success');
+    const { query } = useRouter();
+    useEffect( () => {
+      
+        if(router.query.type){
+           
+            if(router.query.type == 'success'){
+               setType('success');
+                
+            }else{
+                setType('error');
 
+            }
+            setMessage(query['message'])
+            setShowToast(true);
+        }
+       
+
+    })
+    const handleCloseToast = () =>{
+        setShowToast(false);
+        setMessage(null)
+        router.push(`/feature/detail/${slug}`)
+
+        delete router.query.success;
+
+   }
     if(props.data.statusCode !== 200){
         router.push(
             { pathname: "/feature", query: { message: props.data.message,type:'error' } },"/feature"
@@ -42,7 +69,7 @@ const Detail = (props) => {
         </div>
         <div className="flex  flex-col sm:flex-col md:flex-col lg:flex-row xl:flex-row">
             <div className="w-full  sm:w-auto md:w-full lg:w-3/4 xl:w-3/4 ">
-                <div className="px-4 py-3 m-2  bg-white rounded-lg shadow-md dark:bg-gray-800">
+                <div className="relative px-4 py-3 m-2  bg-white rounded-lg shadow-md dark:bg-gray-800">
                     <h1 className="text-gray-800 text-3xl font-bold ">{props.data.feature.name}</h1>
                     <p className="text-gray-500 mt-0 text-sm font-thin">
                         {props.data.feature.description}
@@ -67,6 +94,7 @@ const Detail = (props) => {
                                         
                                     </ul>
                                     }
+                <Link className="absolute bottom-3 right-3 " href={`/feature/resource/edit/${slug}`}>Edit Resource</Link>
                    
                 </div>
             </div>
@@ -81,7 +109,7 @@ const Detail = (props) => {
                                          <span className="ml-1 text-sm font-normal text-gray-500 dark:text-gray-400">/{value.duration}</span>
                                         
                                 </div>
-                                <Link href={`/feature/price/${props.data.feature.slug}/${value.slug}`} className="absolute top-0 right-0 ">
+                                <Link href={`/feature/price/${props.data.feature.slug}/${value.slug}`} className="absolute top-2 right-2 ">
                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                         </svg>
@@ -97,7 +125,25 @@ const Detail = (props) => {
             </div>
 
         </div>
+        {showToast &&  <div id="toast-success" className="flex absolute top-20 right-5 items-center p-4 mb-4 w-full max-w-xs text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+                
+                    <div className={(type == 'success' ? " text-green-500 bg-green-100 " : " text-red-500 bg-red-200 ") + "inline-flex flex-shrink-0 justify-center items-center w-8 h-8 rounded-lg dark:bg-green-800 dark:text-green-200"}>
+                
+                        {type == 'success' && <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg> }
+                        {type == 'error' && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                            </svg>
+                        }
+                        <span className="sr-only">Check icon</span>
+                    </div>
+                    <div className="ml-3 text-sm font-normal">{query['message']}</div>
+                    <button type="button" onClick={() => handleCloseToast()} className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
+                        <span className="sr-only">Close</span>
+                        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </button>
         
+        
+                </div> }
     </MainLayout>
   )
 }
--- pages/feature/price/[slug]/[price].js
+++ pages/feature/price/[slug]/[price].js
@@ -1,9 +1,193 @@
-import React from 'react'
+import { useRouter } from "next/router"
+import api from "../../../../lib/Api";
+import { useAuth } from "../../../../contexts/auth";
+import MainLayout from "../../../../layouts/MainLayout";
+import { useState } from "react";
+import Label from "../../../../components/Label";
+import Input from "../../../../components/Input";
+import Select from "../../../../components/Select";
+import Button from "../../../../components/Button";
+import Cookies from "js-cookie";
 
-const Edit = () => {
+const Edit = (props) => {
+  console.log(props)
+
+  const router = useRouter();
+  const {slug,price} = router.query;
+  const {user,level} = useAuth();
+  const breadcrumb = [{'name':'Feature','url':'/feature'},{'name':'Detail','url':`/feature/detail/${slug}`},{'name':'Edit Price',url:`/feature/price/${slug}/${price}`}];
+  const [name,setName] = useState(props.data.price.name)
+  const [duration,setDuration] = useState(props.data.price.duration)
+  const [discount,setDiscount] = useState(props.data.price.discount)
+  const [prices,setPrice] = useState(props.data.price.price)
+  const [errors,setError] = useState([]);
+  const [loading,setLoading] = useState(false);
+
+
+  if(props.data.statusCode !== 200){
+    router.push(
+      { pathname: "/feature", query: { message: props.data.message,type:'error' } },"/feature"
+     
+    );
+  }
+
+  if(level !== 'admin'){
+    router.push(
+      {pathname:"/feature",query:{message:'You cant access this page',type:'error'}},"/feature"
+    )
+  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setLoading(true)
+
+    const token = Cookies.get('token')
+    api.defaults.headers.Authorization = `Bearer ${token}`
+    api
+    .post(`/api/feature/edit/price/${slug}/${price}`,{name,discount,price:prices,duration})
+    .then((res) =>{
+        
+        setLoading(false)
+       
+        if(res.data.statusCode == 200){
+            router.push(
+                { pathname: `/feature/detail/${slug}`, query: { message: "Feature price successfuly updated",type:'success' } },`/feature/detail/${slug}`
+               
+              );
+        }else{
+            setError(res.data.message);
+           
+        }
+    })
+
+  }
   return (
-    <div>Edit</div>
+    <MainLayout user={user} breadcrumb={breadcrumb} title="Edit Price" page="Feature">
+        <div className='flex justify-between mb-0'>
+            <h4 className="mb-4 text-lg font-semibold text-gray-400 dark:text-gray-300">
+               Edit Price 
+            </h4>
+        </div>
+        <div className="px-4 py-3   bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <form onSubmit={submitHandler}>
+              <div className="flex flex-no-wrap">
+                    <Label className="w-2/5 block text-sm mb-4 mt-4">
+                        <span className="text-gray-700 dark:text-gray-400">
+                            Name
+                        </span>
+                        <Input 
+                            type="text" 
+                            className={ (errors.name ? ' border-red-300 focus:border-red-400' : ' border-indigo-100 focus:border-indigo-400') +  "  block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-sm"}
+                        
+                            value={name}
+                            placeholder="Insert price name"
+                            onChange = { (e) => setName(e.target.value)}
+                            disabled={loading == false ? '':true}
+                            
+                        >
+                        </Input>
+                        {errors && errors.name &&
+                            errors.name.map((value,index) => {
+                                return (
+                                    <p className="mt-2 text-sm text-red-600 dark:text-red-500" key={index}><span className="font-medium">Oops!</span> {value} </p>
+                                )
+                            })
+                        }
+                    </Label>
+                    <Label className="w-2/5 block text-sm mb-4 mt-4 mx-4">
+                        <span className="text-gray-700 dark:text-gray-400">
+                            Duration
+                        </span>
+                        <Select 
+                            className={ (errors.duration ? ' border-red-300 focus:border-red-400' : ' border-indigo-100 focus:border-indigo-400') +  "  block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-sm"}
+                            value={duration}
+                            disabled={loading == false ? '':true}
+                            onChange={ (e) => setDuration(e.target.value)}
+                            
+                        >
+                        <option value="daily">Daily</option>
+                        <option value="weekly">Weekly</option>
+                        <option value="monthly">Monthly</option>
+                        <option value="yearly">Yearly</option>
+                        </Select>
+                    </Label>
+                    {errors && errors.duration &&
+                      errors.duration.map((value,index) => {
+                            return (
+                                <p className="mt-2 text-sm text-red-600 dark:text-red-500" key={index}><span className="font-medium">Oops!</span> {value} </p>
+                            )
+                        })
+                    }
+                </div>
+                <div className="flex flex-no-wrap">
+                    <Label className="w-2/5 block text-sm mb-4 mt-4">
+                        <span className="text-gray-700 dark:text-gray-400">
+                            Price
+                        </span>
+                        <Input 
+                            type="number" 
+                            className={ (errors.price ? ' border-red-300 focus:border-red-400' : ' border-indigo-100 focus:border-indigo-400') + " block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-sm"}
+                            value={prices}
+                            placeholder="Insert price"
+                            onChange={ (e) => setPrice(e.target.value)}
+                            disabled={loading == false ? '':true}
+                          
+                        >
+                        </Input>
+                        {errors && errors.price &&
+                          errors.price.map((value,index) => {
+                                return (
+                                    <p className="mt-2 text-sm text-red-600 dark:text-red-500" key={index}><span className="font-medium">Oops!</span> {value} </p>
+                                )
+                            })
+                        }
+                    </Label>
+                    <Label className="w-2/5 block text-sm mb-4 mt-4 mx-4">
+                        <span className="text-gray-700 dark:text-gray-400">
+                            Discount
+                        </span>
+                        <Input 
+                            type="number" 
+                            className={ (errors.discount ? ' border-red-300 focus:border-red-400' : ' border-indigo-100 focus:border-indigo-400') + " block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-sm"}
+                            value={discount}
+                            placeholder="Insert discount"
+                            onChange={ (e) => setDiscount(e.target.value)}
+                            disabled={loading == false ? '':true}
+                            
+                        >
+                        </Input>
+                        {errors && errors.discount &&
+                          errors.discount.map((value,index) => {
+                            return (
+                                <p className="mt-2 text-sm text-red-600 dark:text-red-500" key={index}><span className="font-medium">Oops!</span> {value} </p>
+                            )
+                        })
+                    }
+                    </Label>
+                </div>
+                {!loading &&   <Button  className="float-right inline-block px-6 py-2.5 bg-indigo-400 text-white font-medium text-xs leading-tight rounded-full shadow-md hover:bg-indigo-800 hover:shadow-lg focus:bg-indigo-800  focus:outline-none focus:ring-0 transition duration-150 uppercase ">Submit</Button>}
+                {loading &&   <Button disabled={true} className="float-right px-6  disabled:text-white text-xs  font-medium disabled:bg-indigo-500  rounded-full btn btn-sm  loading">Loading</Button>}
+              </form>
+          </div>
+    </MainLayout>
   )
 }
 
+
+export async function getServerSideProps(context) {
+  const token = context.req.cookies.token;
+  const slug = context.query.slug;
+  const price = context.query.price;
+  api.defaults.headers.Authorization = `Bearer ${token}`
+  const { data: resp } = await api.get(`/api/feature/edit/price/${slug}/${price}`)
+   
+   
+  if (resp){
+      return {
+          props:{
+              data:resp
+          },
+      }
+  } 
+  
+}
 export default Edit
