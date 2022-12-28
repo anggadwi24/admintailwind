ea54c46113df2e5029ffe219519d64dfd7f4eb3f
anggadwi24 <anggadwi1014@gmail.com>
2022-12-28 15:25:01 +0800

    dear

--- pages/company/add.js
+++ pages/company/add.js
@@ -31,21 +31,27 @@ const Add = () => {
     const [address,setAddress] = useState('')
     const [postcode, setPostcode] = useState('')
     const [category,setCategory] = useState('')
+    const [feature,setFeature] = useState('default')
+    const [price,setPrice] = useState('default')
+    const [method,setMethod] = useState('')
 
 
     const [selectCity,setSelectCity] = useState([]);
+    const [selectPrice,setSelectPrice] = useState([]);
   
     const [errors,setErrors] = useState([])
     const breadcrumb = [{'name':'Company','url':'/company'},{'name':'Add','url':'/company/add'}];
     const {data:categories,errorc} = useSWR([`https://kasirku.juastudio.com/api/category`,token],fetcher);
    
-    const [query, setQuery] = useState('')
+    
    
     
 
     
      
     const {data:provinces,error} = useSWR([`https://kasirku.juastudio.com/api/province`,token],fetcher);
+    const {data:features,errorf} = useSWR([`https://kasirku.juastudio.com/api/featureall`,token],fetcher);
+
     
 
    
@@ -53,7 +59,7 @@ const Add = () => {
        
         setProvince(event.target.value);
       
-        api.defaults.headers.Authorization = `Bearer ${token}`
+       
       
         var country_select = document.querySelector("#province");
         var key = country_select.options[country_select.selectedIndex].getAttribute('index');
@@ -65,6 +71,16 @@ const Add = () => {
       
         
     }
+
+    const handleFeature = (event) => {
+        setFeature(event.target.value)
+        var country_select = document.querySelector("#feature");
+        var key = country_select.options[country_select.selectedIndex].getAttribute('index');
+       
+      
+        setSelectPrice([features.data[key].price])
+    }
+
     
   
   return (
@@ -73,90 +89,137 @@ const Add = () => {
                 <h4 className="mb-4 text-lg font-semibold text-gray-400 dark:text-gray-300">Add Company</h4>
             </div>
            
-            <form className="px-4 py-3   bg-white rounded-lg shadow-md dark:bg-gray-800  ">
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                            Company Name
-                        </Label>
-                        <Input value={name} onChange={ (e) => setName(e.target.value)} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} id="grid-first-name" type="text" placeholder="Company Name"/>
-                        {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
-                    </div>
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                            Category
-                        </Label>
-                        {categories && categories.record.length > 0 && 
-                            <Autocomplete options={categories.record} 
-                            value={category}
-                            onChange={setCategory}
-                        />
-                        }
-                        
-                    </div>
-                    
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full md:w-2/6 px-3 mb-6 md:mb-0">
-                        <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                            Email
-                        </Label>
-                        <Input value={email} onChange={ (e) => setEmail(e.target.value)} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} id="grid-first-name" type="text" placeholder="company@gmail.com"/>
-                      
-                    </div>
-                    <div className="w-full md:w-2/6 px-3 mb-6 md:mb-0">
-                        <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                            Provinsi
-                        </Label>
-                        <Select id="province" onChange={  handleProvince}  value={province} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} >
-                            <option disabled={true} value="default">{!provinces ? 'Loading..':'Select provinces'}</option>
-                            {provinces && provinces.data.length > 0 && 
-                                provinces.data.map( (value,index) => {
-                                    return (
-                                        <option value={value.id} index={index} key={value.id}>{value.name}</option>
-                                    )
-                                })
+            <form className="   ">
+                <div className='px-4 py-3  bg-white rounded-lg shadow-md dark:bg-gray-800'>
+
+              
+                    <h4 className="mb-8 text-lg font-semibold text-dark uppercase dark:text-gray-300">Data Company</h4>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Company Name
+                            </Label>
+                            <Input value={name} onChange={ (e) => setName(e.target.value)} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} id="grid-first-name" type="text" placeholder="Company Name"/>
+                            {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
+                        </div>
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Category
+                            </Label>
+                            {categories && categories.record.length > 0 && 
+                                <Autocomplete options={categories.record} 
+                                value={category}
+                                onChange={setCategory}
+                            />
                             }
-                        </Select>
-                       
-                    </div>
-                    <div className="w-full md:w-2/6 px-3 mb-6 md:mb-0">
-                        <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                            City
-                        </Label>
-                        <Select id="province" onChange={  (e) => setCity(e.target.value)}  value={city} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} >
-                            <option disabled={true} value="default">Select city</option>
-                            { selectCity[0] && selectCity[0].length > 0 && 
-                                selectCity[0].map( (value,index) => {
-                                   
-                                    return (
-                                        <option value={value.id} index={index} key={value.id}>{value.name}</option>
-                                    )
-                                })
-                            }
-                            {selectCity.length === 0 && 
-                                <option disabled={true} value="null">Select province first</option>
-                            }
-                        </Select>
-                      
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                            Address
-                        </Label>
-                        <Textarea value={address} onChange={ (e) => setAddress(e.target.value)} className={"  border-indigo-100 focus:ring-indigo-400 focus:border-indigo-400block p-2.5 text-sm mt-1 w-full  border  rounded-lg   focus:outline-none"} id="grid-first-name" placeholder='Company address'>
                             
-                        </Textarea>
+                        </div>
                         
                     </div>
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                            Post Code
-                        </Label>
-                        <Input value={postcode} onChange={ (e) => setPostcode(e.target.value)} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} id="grid-first-name" type="number" placeholder="XXXXX"/>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full md:w-2/6 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Email
+                            </Label>
+                            <Input value={email} onChange={ (e) => setEmail(e.target.value)} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} id="grid-first-name" type="text" placeholder="company@gmail.com"/>
                         
+                        </div>
+                        <div className="w-full md:w-2/6 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Provinsi
+                            </Label>
+                            <Select id="province" onChange={  handleProvince}  value={province} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} >
+                                <option disabled={true} value="default">{!provinces ? 'Loading..':'Select provinces'}</option>
+                                {provinces && provinces.data.length > 0 && 
+                                    provinces.data.map( (value,index) => {
+                                        return (
+                                            <option value={value.id} index={index} key={value.id}>{value.name}</option>
+                                        )
+                                    })
+                                }
+                            </Select>
+                        
+                        </div>
+                        <div className="w-full md:w-2/6 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                City
+                            </Label>
+                            <Select onChange={  (e) => setCity(e.target.value)}  value={city} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} >
+                                <option disabled={true} value="default">Select city</option>
+                                { selectCity[0] && selectCity[0].length > 0 && 
+                                    selectCity[0].map( (value,index) => {
+                                    
+                                        return (
+                                            <option value={value.id} index={index} key={value.id}>{value.name}</option>
+                                        )
+                                    })
+                                }
+                                {selectCity.length === 0 && 
+                                    <option disabled={true} value="null">Select province first</option>
+                                }
+                            </Select>
+                        
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Address
+                            </Label>
+                            <Textarea value={address} onChange={ (e) => setAddress(e.target.value)} className={"  border-indigo-100 focus:ring-indigo-400 focus:border-indigo-400block p-2.5 text-sm mt-1 w-full  border  rounded-lg   focus:outline-none"} id="grid-first-name" placeholder='Company address'>
+                                
+                            </Textarea>
+                            
+                        </div>
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Post Code
+                            </Label>
+                            <Input value={postcode} onChange={ (e) => setPostcode(e.target.value)} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} id="grid-first-name" type="number" placeholder="XXXXX"/>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className='px-4 py-3 mt-4 bg-white rounded-lg shadow-md dark:bg-gray-800'>
+                    <h4 className="mb-8 text-lg font-semibold text-dark uppercase dark:text-gray-300">Data Feature</h4>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                       
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Feature
+                            </Label>
+                            <Select id="feature" onChange={  handleFeature}  value={feature} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} >
+                                <option disabled={true} value="default">{!features ? 'Loading..':'Select Feature'}</option>
+                                {features && features.data.length > 0 && 
+                                    features.data.map( (value,index) => {
+                                        return (
+                                            <option value={value.slug} index={index} key={value.slug}>{value.name}</option>
+                                        )
+                                    })
+                                }
+                            </Select>
+                        
+                        </div>
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                Duration
+                            </Label>
+                            <Select  onChange={  (e) => setPrice(e.target.value)}  value={price} className={"  border-indigo-100 focus:border-indigo-400 block w-full  mt-1 text-sm dark:text-gray-300 dark:bg-gray-700  focus:outline-none focus:shadow-outline-red input input-md"} >
+                                <option disabled={true} value="default">Select price</option>
+                                { selectPrice[0] && selectPrice[0].length > 0 && 
+                                    selectPrice[0].map( (value,index) => {
+
+                                        return (
+                                            <option value={value.slug} index={index} key={value.slug}>{value.name} - {value.discount > 0 ? (value.price-value.discount).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') : value.price.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') }</option>
+                                        )
+                                    })
+                                }
+                                {selectPrice.length === 0 && 
+                                    <option disabled={true} value="null">Select price first</option>
+                                }
+                            </Select>
+                        
+                        </div>
                     </div>
                 </div>
             </form>
